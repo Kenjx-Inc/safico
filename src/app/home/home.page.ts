6b0db2d746d07e991f3d8d3cce40554434b9ba2b
KenjxJJ <kenjxjj@gmail.com>
2021-10-28 02:24:59 +0300

    Add first look to the home ui

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,11 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
+
 export class HomePage implements OnInit {
+  options = {
+    slidesPerView: 1.5,
+    centered: true,
+    spaceBetweenView: 10,
+    autoHeight: true
+  };
+
+  slidesOptions = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    speed: 400
+  };
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
