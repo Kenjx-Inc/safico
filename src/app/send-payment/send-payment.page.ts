9d35a4c5085f34d5f11807842e7562059c068435
KenjxJJ <kenjxjj@gmail.com>
2021-11-26 23:37:05 +0300

    Add a toast message in send payment

--- src/app/send-payment/send-payment.page.ts
+++ src/app/send-payment/send-payment.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CallNumber } from '@ionic-native/call-number/ngx';
+import { ToastController } from '@ionic/angular';
 import { DataService } from '../services/data.service';
 import { MobileMoneyApiService } from '../services/mobile-money-api.service';
+
 
 @Component({
   selector: 'app-send-payment',
@@ -14,41 +16,56 @@ export class SendPaymentPage implements OnInit {
   codeForAirtelMobileMoney: any;
   orderTotal: number;
   cartOrder: any;
+  userID: any;
   orderPaymentSent: boolean;
 
   constructor(private dataService: DataService,
     private call: CallNumber,
     private mobileMoneyAPI: MobileMoneyApiService,
-    private router: Router) { }
+    private router: Router,
+    private toastController: ToastController) {
+    const userInfo = JSON.parse(localStorage.getItem('user'));
+    this.userID = userInfo.uid;
+
+  }
 
   ngOnInit() {
-    this.dataService.getCartOrder()
+    // Obtain the total price
+    this.dataService.getCartOrder(this.userID)
       .subscribe((res) => {
-        this.cartOrder = res.map((order) => {
-          return order.payload.doc.data()
-        });
-        console.log(this.cartOrder[0]);
-        this.orderTotal = this.cartOrder[0].total;
-      })
+        this.orderTotal = res.payload.get('total');
+      });
   }
 
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your order is being processed...',
+      duration: 2000
+    });
+    toast.present();
+
+    toast.onWillDismiss().then(() => {
+      this.router.navigate(['/track-orders']);
+    });
+
+  }
 
   initiatePaymentViaAitel() {
-    // // Call to Airtel Payment   
-    this.mobileMoneyAPI.makePaymentViaAitel().subscribe((res) => {
-      console.log(res);
-      console.log("RES", res);
-    })
-
+    // // Call to Airtel Payment
+    // this.mobileMoneyAPI.makePaymentViaAitel().subscribe((res) => {
+    //   console.log(res);
+    //   // console.log("RES", res);
+    // });
+    this.presentToast();
   }
 
-  initiatePaymentMTN() {
-    // // Call to Airtel Payment   
+  initiatePaymentViaMTN() {
+    // // Call to Airtel Payment
     // this.mobileMoneyAPI.makePaymentViaMTN().subscribe( (res)=>{
     //      console.log(res);
     //      console.log("RES", res);
     //  })
-
+    this.presentToast();
   }
 }
 
